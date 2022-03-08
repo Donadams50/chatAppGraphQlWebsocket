da83fa5eafc8d99f87f82620127a20048eff9cb5
Donadams50 <sumbomatic@gmail.com>
2022-03-08 16:47:11 +0100

    first commit

---
+++ client/src/Chat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+ 
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
+  } from "@apollo/client";
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache()
+  });
+  
